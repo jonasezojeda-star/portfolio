3448054136707f0bd5b8b412265aec35abffe0d6
jonas ojeda <jonojed98@gmail.com>
2025-11-25 23:50:40 -0300

    feat: Implementacion de recurso Skills en filament

---
+++ Back/app/Filament/Resources/SkillsResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SkillsResource\Pages;
+use App\Filament\Resources\SkillsResource\RelationManagers;
+use App\Models\Skills;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SkillsResource extends Resource
+{
+    protected static ?string $model = Skills::class;
+
+    protected static ?string $navigationIcon = 'hugeicons-geometric-shapes-01';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSkills::route('/'),
+            'create' => Pages\CreateSkills::route('/create'),
+            'edit' => Pages\EditSkills::route('/{record}/edit'),
+        ];
+    }
+}
