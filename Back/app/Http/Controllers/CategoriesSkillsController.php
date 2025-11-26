1994a135c7455865de39a15841e5c9ed658ee4ff
jonas ojeda <jonojed98@gmail.com>
2025-11-25 23:41:01 -0300

    feat: Implementacion de migracion y modelo CategoriesSkills y Skills

---
+++ Back/app/Http/Controllers/CategoriesSkillsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CategoriesSkills;
+use Illuminate\Http\Request;
+
+class CategoriesSkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoriesSkills $categoriesSkills)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoriesSkills $categoriesSkills)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CategoriesSkills $categoriesSkills)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoriesSkills $categoriesSkills)
+    {
+        //
+    }
+}
